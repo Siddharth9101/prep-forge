1f1824da668bb174721392d843e152a91c3cd94a
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-28 19:37:33 +0530

    implement auth service layer

---
+++ frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000/api/v1/auth",
+  withCredentials: true,
+});
+
+/**
+ * @params {username, email, password}
+ * @returns user info
+ * @description registers new user and sets a token cookie
+ */
+export async function register({ username, email, password }) {
+  try {
+    const response = await api.post("/register", { username, email, password });
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * @params {email, password}
+ * @returns user info
+ * @description logs in user and sets a token cookie
+ */
+export async function login({ email, password }) {
+  try {
+    const response = await api.post("/login", { email, password });
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * @description logs out user, clears token cookie
+ */
+export async function logout() {
+  try {
+    const response = await api.get("/logout");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * @description returns current logged in user's info
+ */
+export async function getMe() {
+  try {
+    const response = await api.get("/get-me");
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
