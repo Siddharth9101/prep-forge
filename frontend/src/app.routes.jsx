df57272ab7303bd14e2e240095fdd08052f7336c
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-28 20:51:16 +0530

    protect home route

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login.jsx";
 import Register from "./features/auth/pages/Register.jsx";
+import Protected from "./features/auth/components/Protected.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
   },
   {
     path: "/",
-    element: <h1>HomePage</h1>,
+    element: (
+      <Protected>
+        <h1>HomePage</h1>
+      </Protected>
+    ),
   },
 ]);
