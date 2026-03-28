1071550a47a9bb6e5a5b2bff29a06f89b0011c8f
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-28 11:20:09 +0530

    add auth controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,146 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+/**
+ * @name registerUserController
+ * @description Register a new user, expects username, email, and password in the request body
+ * @access Public
+ */
+async function registerUserController(req, res) {
+  try {
+    const { username, email, password } = req.body;
+
+    // Basic validation
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username, email, and password are required" });
+    }
+
+    // Check if user with this username or email already exists
+    const existingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (existingUser) {
+      if (existingUser.username === username) {
+        return res.status(400).json({ message: "Username already taken" });
+      }
+      return res
+        .status(400)
+        .json({ message: "Account already exists with email" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save the new user to the database
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    //Set token to cookie
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+        username: newUser.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
+    // Return success with user
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: newUser._id,
+        email: newUser.email,
+        username: newUser.username,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+/**
+ * @name loginUserController
+ * @description Login user, expects email and password in the request body
+ * @access Public
+ */
+async function loginUserController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // Basic validation
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    // Check if user with this email exists
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Set token to cookie
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+        username: newUser.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
+    // Return success with user
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: existingUser._id,
+        email: existingUser.email,
+        username: existingUser.username,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+module.exports = {
+  registerUserController,
+  loginUserController,
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,20 @@
 const { Router } = require("express");
+const authController = require("../controllers/auth.controller");
 
 const authRouter = Router();
+
+/**
+ * @route POST /api/v1/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/v1/auth/login
+ * @description Login a user
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController);
 
 module.exports = authRouter;
