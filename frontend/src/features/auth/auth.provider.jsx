74e890b225fee0f5b35900718e5fe90b532feb57
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-28 21:05:36 +0530

    refactor: enhance AuthContextProvider to fetch user data on mount

--- frontend/src/features/auth/auth.provider.jsx
+++ frontend/src/features/auth/auth.provider.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./auth.context.js";
+import { getMe } from "./services/auth.api.js";
 
 export default function AuthContextProvider({ children }) {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getMe();
+        if (data?.user) {
+          setUser(data.user);
+        } else {
+          setUser(null);
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
