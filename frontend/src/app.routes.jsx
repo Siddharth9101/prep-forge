44e1347d1cf3c5ce28e9a5f295b4e097e5667096
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-30 11:22:42 +0530

    add interview page ui

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login.jsx";
 import Register from "./features/auth/pages/Register.jsx";
 import Protected from "./features/auth/components/Protected.jsx";
+import Home from "./features/interview/pages/Home.jsx";
+import Interview from "./features/interview/pages/Interview.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -16,7 +18,15 @@ export const router = createBrowserRouter([
     path: "/",
     element: (
       <Protected>
-        <h1>HomePage</h1>
+        <Home />
+      </Protected>
+    ),
+  },
+  {
+    path: "/interview/:id",
+    element: (
+      <Protected>
+        <Interview />
       </Protected>
     ),
   },
