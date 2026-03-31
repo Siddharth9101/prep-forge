f6163855ad434ddbaf7e768124846c7ca2e56cdc
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-03-31 16:45:54 +0530

    update backend url in frontend

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/interview",
+  baseURL: "https://prep-forge.onrender.com/api/v1/interview",
   withCredentials: true,
 });
 
