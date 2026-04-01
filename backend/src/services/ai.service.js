a861eda870890c8b88f7ed2d11995dee9f78766f
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-04-01 13:53:30 +0530

    fix

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -133,8 +133,6 @@ async function generateInterviewReport({
 async function generatePdfFromHtml(htmlContent) {
   const browser = await puppeteer.launch({
     headless: "new",
-    executablePath:
-      "/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.153/chrome",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
